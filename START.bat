6a67b85f99c816d832cf013928469702e28ad35c
Стефан Йовев <stefanyovev@users.noreply.github.com>
2024-01-12 23:25:07 +0200

    portaudio.dll

--- START.bat
+++ START.bat
@@ -2,7 +2,7 @@
 @set LIBRARY_PATH=%~dp0%lib
 @set PATH=%PATH%%LIBRARY_PATH%;
 
-@tcc -lRTFIR -luser32 -lgdi32 main.c -o RTFIR.exe
+@tcc -lportaudio -luser32 -lgdi32 main.c -o RTFIR.exe
 
 @if %errorlevel% neq 0 goto error
 
