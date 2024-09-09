0739588f5ce832467973c383ac5579b99fa5923d
Стефан Йовев <stefanyovev@users.noreply.github.com>
2024-09-09 18:05:20 +0300

    Link convolve.dll

--- START.bat
+++ START.bat
@@ -2,7 +2,7 @@
 @set LIBRARY_PATH=%~dp0%lib
 @set PATH=%PATH%%LIBRARY_PATH%;
 
-@tcc -lportaudio -luser32 -lgdi32 main.c -o RTFIR.exe
+@tcc -lportaudio -lconvolve -luser32 -lgdi32 main.c -o RTFIR.exe
 
 @if %errorlevel% neq 0 goto error
 
