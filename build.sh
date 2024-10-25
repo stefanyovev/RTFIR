ad39b7b272a09e437680be20a4bdd22ad2553a5c
stefanyovev <notmyemail@really.com>
2024-10-26 02:12:07 +0300

    cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 
 # msys2
 
-pacman -S mingw-w64-ucrt-x86_64-gcc --needed
-pacman -S make --needed
-pacman -S cmake --needed
+if [ ! -d cache ]; then mkdir cache; fi
+
+if [ ! -f cache/sysok ]; then
+    pacman -S mingw-w64-ucrt-x86_64-gcc --needed
+    pacman -S make --needed
+    pacman -S cmake --needed
+    touch cache/sysok
+fi
 
 cp -rf main.c portaudio
 cp -rf conf.c portaudio
@@ -12,14 +17,14 @@ cp -rf main.ico portaudio
 echo "
 main.ico ICON \"main.ico\"
 1 VERSIONINFO
-FILEVERSION     1,0,0,0
+FILEVERSION     2024,10,26,1
 BEGIN
   BLOCK \"StringFileInfo\"
   BEGIN
     BLOCK \"080904E4\"
     BEGIN      
       VALUE \"ProductName\", \"RTFIR\"
-      VALUE \"ProductVersion\", \"9\"
+      VALUE \"ProductVersion\", \"10\"
       VALUE \"LegalCopyright\", \"RTFIR.com\"
       VALUE \"OriginalFilename\", \"RTFIR.exe\"
       VALUE \"FileDescription\", \"Realtime Sound EQ\"
@@ -33,19 +38,19 @@ END
 " > portaudio/main.rc
 
 # -------------------------------------------------
-if [ ! -f portaudio/CMakeLists.txt.changed ]; then
+if [ ! -f cache/CMakeLists_changed ]; then
     echo "
     add_compile_options(-std=c99 -march=native -O3 -msse3 -mavx -fno-aggressive-loop-optimizations -fPIC -fpermissive -w)
     add_executable(RTFIR main.c main.rc)
     target_link_libraries(RTFIR PortAudio gdi32 stdc++ -mwindows)
-    " > portaudio/CMakeLists.txt.changed
-    cat portaudio/CMakeLists.txt.changed >> portaudio/CMakeLists.txt
+    " >> portaudio/CMakeLists.txt
+    touch cache/CMakeLists_changed
 fi
 
 # -------------------------------------------------
 cd portaudio
-rm -f CMakeCache.txt
-rm -rf CMakeFiles
+#rm -f CMakeCache.txt
+#rm -rf CMakeFiles
 cmake -D WIN32=1 -D PA_USE_ASIO=ON -D PA_USE_WMME=OFF -D PA_USE_DS=OFF -D BUILD_SHARED_LIBS=OFF -D LINK_PRIVATE_SYMBOLS=ON .
 cmake --build .
 if [ $? -ne 0  ]; then exit; fi;
